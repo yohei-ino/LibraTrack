f3e6bfbfba85265532d1a6548e6dff5d6e2fc7d4
yohei-ino <yohei.ino.8073@gmail.com>
2025-05-17 05:39:36 +0900

    [chore]: Initialize project with Spring Initializr

---
+++ src/main/kotlin/com/example/libratrack/LibratrackApplication.kt
@@ -0,0 +1,11 @@
+package com.example.libratrack
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibratrackApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibratrackApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/libratrack/LibratrackApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.libratrack
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibratrackApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
