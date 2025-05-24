8867b2dd88659928f109ce543b8d3b93f20e6429
yohei-ino <yohei.ino.8073@gmail.com>
2025-05-24 19:29:23 +0900

    [feat]: Implement book update functionality

--- src/main/kotlin/com/libratrack/controller/BookController.kt
+++ src/main/kotlin/com/libratrack/controller/BookController.kt
@@ -1,6 +1,8 @@
 package com.libratrack.controller
 
+import com.libratrack.dto.Book
 import com.libratrack.dto.BookInput
+import com.libratrack.dto.BookUpdate
 import com.libratrack.service.BookService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -13,5 +15,11 @@ class BookController(private val bookService: BookService) {
     fun createBook(@RequestBody bookInput: BookInput): ResponseEntity<Int> {
         val bookId = bookService.createBook(bookInput)
         return ResponseEntity.ok(bookId)
+    }
+
+    @PutMapping
+    fun updateBook(@RequestBody bookUpdate: BookUpdate): ResponseEntity<Book> {
+        val book = bookService.updateBook(bookUpdate)
+        return ResponseEntity.ok(book)
     }
 } 
