dfc0656438275d10cc2b3cf7b077302e8aac31cc
yohei-ino <yohei.ino.8073@gmail.com>
2025-05-27 08:56:00 +0900

    [feat]: Add validation for book update

--- src/main/kotlin/com/libratrack/dto/AuthorUpdate.kt
+++ src/main/kotlin/com/libratrack/dto/AuthorUpdate.kt
@@ -1,9 +1,15 @@
 package com.libratrack.dto
 
 import java.time.LocalDate
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Past
 
 data class AuthorUpdate(
     val id: Int,
+
+    @field:NotBlank(message = "名前は必須です")
     val name: String,
+
+    @field:Past(message = "生年月日は過去の日付である必要があります")
     val birthDate: LocalDate
 ) 
